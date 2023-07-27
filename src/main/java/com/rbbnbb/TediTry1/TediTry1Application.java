2882d8add8c247dea4a0e3a3551f4a9470d428e6
askertsos <aggelosskertsos@gmail.com>
2023-07-27 18:56:05 +0300

    Initial Commit

---
+++ src/main/java/com/rbbnbb/TediTry1/TediTry1Application.java
@@ -0,0 +1,13 @@
+package com.rbbnbb.TediTry1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TediTry1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TediTry1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbbnbb/TediTry1/TediTry1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbbnbb.TediTry1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TediTry1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
