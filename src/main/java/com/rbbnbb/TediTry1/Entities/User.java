2eafddb74125e504a259c2afbf4ebe9ea2b92f97
etothepowerofipi <nikwnbibis@gmail.com>
2023-07-31 19:15:41 +0300

    user boilerplate

--- src/main/java/com/rbbnbb/TediTry1/Entities/User.java
+++ src/main/java/com/rbbnbb/TediTry1/Entities/User.java
@@ -1,11 +1,92 @@
 package com.rbbnbb.TediTry1.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
+    enum roles {ADMIN, HOST, TENANT, HOST_TENANT};
+    private roles role;
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private Boolean isHost;
+    private String email;
+    //private Photo picture
+    private String phoneNumber;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Boolean getHost() {
+        return isHost;
+    }
+
+    public void setHost(Boolean host) {
+        isHost = host;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public roles getRole() {
+        return role;
+    }
+
+    public void setRole(roles role) {
+        this.role = role;
+    }
 }
