08bdf5cdda8273c300493c5132c9dc7dc86903b8
askertsos <aggelosskertsos@gmail.com>
2023-08-05 19:26:16 +0300

    Almost works

--- src/main/java/com/rbbnbb/TediTry1/configuration/SecurityConfiguration.java
+++ src/main/java/com/rbbnbb/TediTry1/configuration/SecurityConfiguration.java
@@ -1,19 +1,38 @@
 package com.rbbnbb.TediTry1.configuration;
 
-import jdk.jshell.spi.ExecutionControlProvider;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.rbbnbb.TediTry1.utils.RSAKeyProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final RSAKeyProperties keys;
+
+    public SecurityConfiguration(RSAKeyProperties keys){
+        this.keys = keys;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,7 +56,9 @@ public class SecurityConfiguration {
                     auth.requestMatchers("/admin/**").hasRole("ADMIN");
 //                    auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
 //                    auth.anyRequest().permitAll();
-                });
+                })
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 //        http.oauth2ResourceServer()
 //                .jwt()
@@ -48,5 +69,18 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+
 }
 
--- src/main/java/com/rbbnbb/TediTry1/services/AuthenticationService.java
+++ src/main/java/com/rbbnbb/TediTry1/services/AuthenticationService.java
@@ -3,14 +3,20 @@ package com.rbbnbb.TediTry1.services;
 
 import com.rbbnbb.TediTry1.domain.Role;
 import com.rbbnbb.TediTry1.domain.User;
+import com.rbbnbb.TediTry1.dto.LoginResponseDTO;
 import com.rbbnbb.TediTry1.repository.RoleRepository;
 import com.rbbnbb.TediTry1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.naming.AuthenticationException;
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Service
@@ -26,6 +32,12 @@ public class AuthenticationService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     public User registerUser(String username, String password){
         String encodedPassword = passwordEncoder.encode(password);
         Role tenantRole = roleRepository.findByAuthority("ROLE_TENANT").get();
@@ -33,6 +45,22 @@ public class AuthenticationService {
         Set<Role> authorities = new HashSet<>();
         authorities.add(tenantRole);
 
-        return  userRepository.save(new User(username,encodedPassword,authorities));
+        return userRepository.save(new User(username,encodedPassword,authorities));
     }
+
+    public LoginResponseDTO loginUser(String username, String password){
+        try {
+            Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            String token = tokenService.generateJWT(auth);
+            try {
+                return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+            } catch (NoSuchElementException n){
+                System.out.println("Not found");
+                throw new AuthenticationException();
+            }
+        }catch(AuthenticationException e){
+            return new LoginResponseDTO(null, "");
+        }
+    }
+
 }
