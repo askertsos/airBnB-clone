e02fbb2aa39ed5e1ba112a9e48c35471cc76dc5d
etothepowerofipi <nikwnbibis@gmail.com>
2023-08-09 17:19:30 +0300

    works for now

--- web/src/App.js
+++ web/src/App.js
@@ -1,49 +1,48 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
-import Home from './component/home';
-import About from './component/about';
-import Contact from './component/contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./component/home";
+import About from "./component/about";
+import Contact from "./component/contact";
 import LoginPost from "./component/login";
-import './App.css';
+import "./App.css";
 
 function App() {
-  
-  const [jwt,setJwt] = useState(null);
-  const [username,setUsername] = useState(null);
+	const [jwt, setJwt] = useState(null);
+	const [username, setUsername] = useState(null);
 
-  useEffect(() => {
-    const reqBody = {
-      username: "user1",
-      password: "pass1"
-    }
-    fetch("http://localhost:8080/auth/login", { 
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "post",
-      body: JSON.stringify(reqBody)
-    }) 
-      .then((response) => Promise.all([response.json(), response.headers]))
-      .then(([body, headers]) => {
-        setJwt(body.jwt);
-        setUsername(body.user.username);
-        console.log("Server Response Body:", body);
-        console.log("Server Response Headers:", headers);
-      });
-  
-  },[])
+	useEffect(() => {
+		const reqBody = {
+			username: "user1",
+			password: "pass1",
+		};
+		fetch("http://localhost:8080/auth/login", {
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "post",
+			body: JSON.stringify(reqBody),
+		})
+			.then((response) =>
+				Promise.all([response.json(), response.headers])
+			)
+			.then(([body, headers]) => {
+				setJwt(body.jwt);
+				setUsername(body.user.username);
+				console.log("Server Response Body:", body);
+				console.log("Server Response Headers:", headers);
+			});
+	}, []);
 
-
-  return (
-    <Router>
-        <Routes>
-                <Route exact path='/' element={< Home />}></Route>
-                <Route exact path='/about' element={< About />}></Route>
-                <Route exact path='/contact' element={< Contact />}></Route>
-                <Route exact path='/auth/login' element={< LoginPost />}></Route>
-        </Routes>
-    </Router>
-);
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />}></Route>
+				<Route exact path="/about" element={<About />}></Route>
+				<Route exact path="/contact" element={<Contact />}></Route>
+				<Route exact path="/auth/login" element={<LoginPost />}></Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- web/src/component/login.js
+++ web/src/component/login.js
@@ -1,60 +1,62 @@
 import React, { useState } from "react";
 
 const LoginPost = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [jwt,setJwt] = useState(null);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [jwt, setJwt] = useState(null);
 
-  const onSubmit = (e) => {
-    const reqBody = {
-        username: {username},
-        password: {password}
-      }
-      fetch("http://localhost:8080/auth/login", { 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "post",
-        body: JSON.stringify(reqBody)
-      }) 
-        .then((response) => Promise.all([response.json(), response.headers]))
-        .then(([body, headers]) => {
-          setJwt(body.jwt);
-          console.log("Server Response Body:", body);
-          console.log("Server Response Headers:", headers);
-        });
-    };
+	const onSubmit = (e) => {
+		const reqBody = {
+			username: { username },
+			password: { password },
+		};
+		fetch("http://localhost:8080/auth/login", {
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "post",
+			body: JSON.stringify(reqBody),
+		})
+			.then((response) =>
+				Promise.all([response.json(), response.headers])
+			)
+			.then(([body, headers]) => {
+				setJwt(body.jwt);
+				console.log("Server Response Body:", body);
+				console.log("Server Response Headers:", headers);
+			});
+	};
 
-  return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>
-            Username
-            <input
-                id="username"
-                name="username"
-                type="text"
-                placeholder="username"
-                onChange={event => setUsername(event.target.value)}
-                value={username}
-            />
-        </label>
-      </div>
-      <div>
-        <label>
-            Password:
-            <input
-                id="password"
-                name="password"
-                type="text"
-                placeholder="password"
-                onChange={event => setPassword(event.target.value)}
-                value={password}
-            />
-        </label>
-      </div>
-      <button type="submit">Sumbit</button>
-    </form>
-  );
+	return (
+		<form onSubmit={onSubmit}>
+			<div>
+				<label>
+					Username
+					<input
+						id="username"
+						name="username"
+						type="text"
+						placeholder="username"
+						onChange={(event) => setUsername(event.target.value)}
+						value={username}
+					/>
+				</label>
+			</div>
+			<div>
+				<label>
+					Password:
+					<input
+						id="password"
+						name="password"
+						type="text"
+						placeholder="password"
+						onChange={(event) => setPassword(event.target.value)}
+						value={password}
+					/>
+				</label>
+			</div>
+			<button type="submit">Login</button>
+		</form>
+	);
 };
 export default LoginPost;
