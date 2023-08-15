4a80d618f9f45514b104befbf6111e4a7f02b2f9
etothepowerofipi <nikwnbibis@gmail.com>
2023-08-15 20:28:29 +0300

    ela

--- src/main/java/com/rbbnbb/TediTry1/controller/UserController.java
+++ src/main/java/com/rbbnbb/TediTry1/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.rbbnbb.TediTry1.controller;
 
+import com.rbbnbb.TediTry1.repository.UserRepository;
+import com.rbbnbb.TediTry1.repository.UserRepository.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +19,21 @@ public class UserController {
 //    public String helloUserController() {
 //        return "User access level";
 //    }
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    JwtDecoder jwtDecoder;
 
-    @GetMapping("/")
+    @GetMapping("/auth")
     public ResponseEntity<?> authenticateJWT(@RequestHeader("Authorization") String jwt){
-        System.out.println("Token is " + jwt);
-        if (jwt.isBlank() || jwt.isEmpty() || jwt.equals("null") ) return ResponseEntity.status(401).build();
-        return ResponseEntity.ok().build();
+
+//        if (jwt.isBlank() || jwt.isEmpty() || jwt.equals("null") ) return ResponseEntity.status(401).build();
+//        Jwt decodedJWT = jwtDecoder.decode(jwt);
+//        String username = decodedJWT.getSubject();
+//        if (userRepository.findByUsername(username).isPresent())
+            return ResponseEntity.ok().build();
+//        return ResponseEntity.status(401).build();
+
     }
 
 }
--- web/src/component/home.js
+++ web/src/component/home.js
@@ -2,21 +2,20 @@
 import React from "react";
 
 function Home() {
-
 	const loggedIn = localStorage.getItem("jwt");
 
 	return (
 		<>
 			<h1> Welcome to rBBnBB! </h1>
 			<div>
-				{loggedIn != "null" &&
-					<a href = 'http://localhost:3000/auth/logout'>Logout</a>
-				}
+				{loggedIn !== "null" && (
+					<a href="http://localhost:3000/auth/logout">Logout</a>
+				)}
 			</div>
 			<div>
-				{loggedIn == "null" &&
-					<a href = 'http://localhost:3000/auth/login'>Login </a>
-				}
+				{loggedIn === "null" && (
+					<a href="http://localhost:3000/auth/login">Login </a>
+				)}
 			</div>
 		</>
 	);
